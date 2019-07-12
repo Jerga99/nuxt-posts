867697e187d0a1e81b0ca4963bb26d6d80b844bd
Filip Jerga <jerga99@gmail.com>
2019-07-12 22:33:09 +0200

    3 Navbar Component

---
+++ components/Navbar.vue
@@ -0,0 +1,44 @@
+
+<template>
+  <nav class="navbar has-shadow">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="../">
+        <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox">
+        </a>
+        <div class="navbar-burger burger"
+             aria-label="menu"
+             aria-expanded="false"
+             data-target="navMenu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div id="navMenu" class="navbar-menu">
+        <div class="navbar-end">
+          <div class="navbar-item has-dropdown">
+            <a class="navbar-link">
+            Menu
+            </a>
+            <div class="navbar-dropdown">
+              <a class="navbar-item">
+              Dashboard
+              </a>
+              <a class="navbar-item">
+              Profile
+              </a>
+              <a class="navbar-item">
+              Settings
+              </a>
+              <hr class="navbar-divider">
+              <div class="navbar-item">
+                Logout
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,65 @@
 <template>
-  <div class="container">
-    <h1>Me Home Page</h1>
+  <div>
+    <navbar />
+    <div class="blogs-page">
+      <div class="main-content">
+        <div class="container">
+          <div class="columns is-mobile">
+            <div class="column is-8 is-offset-2">
+              <div class="section">
+                <div class="title">
+                  <h1>Newest Posts</h1>
+                  <hr>
+                </div>
+                <div class="post">
+                  <a class="post-header post-header-link clickable">
+                    <h4 class="title is-4">Some Title</h4>
+                    <h5 class="subtitle is-5">Some Subtitle</h5>
+                  </a>
+                  <div class="post-content">
+                    by Filip Jerga, 27th Jan, 2019
+                  </div>
+                </div>
+                <div class="post">
+                  <a class="post-header post-header-link clickable">
+                    <h4 class="title is-4">Some Title</h4>
+                    <h5 class="subtitle is-5">Some Subtitle</h5>
+                  </a>
+                  <div class="post-content">
+                    by Filip Jerga, 27th Jan, 2019
+                  </div>
+                </div>
+              </div>
+              <!-- end of post -->
+            </div>
+            <!-- end of side bar -->
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+import Navbar from '~/components/Navbar'
+export default {
+  components: {
+    Navbar
+  }
+}
 </script>
+<style>
+  .post-content {
+    font-style: italic;
+  }
+
+  .post {
+    margin-bottom: 20px;
+    padding: 5px;
+    border-bottom: 2px solid transparent;
+  }
+
+  .post:hover {
+    border-bottom: 2px solid #e8e8e8;
+  }
+</style>
