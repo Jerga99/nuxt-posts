1f14dc4fd3f34c36f71c2f1a33d92ce8f638b38e
Filip Jerga <jerga99@gmail.com>
2019-07-14 11:14:09 +0200

    9 Methods

--- components/PostItem.vue
+++ components/PostItem.vue
@@ -9,7 +9,7 @@
       <h5 class="subtitle is-5">{{subtitle}}</h5>
     </a>
     <div class="post-content">
-      by Filip Jerga, {{moment(date).format('LL')}}
+      by Filip Jerga, {{formatDate(date)}}
     </div>
   </div>
 </template>
@@ -17,11 +17,6 @@
 <script>
 import moment from 'moment'
 export default {
-  data() {
-    return {
-      moment
-    }
-  },
   props: {
     title: {
       type: String,
@@ -36,9 +31,29 @@ export default {
       required: false,
       default: new Date()
     }
+  },
+  methods: {
+    // test(message) {
+    //   console.log(message)
+    //   return message
+    // },
+    // test2() {
+    //   return this.test(this.title)
+    // }
+    formatDate(date) {
+      return moment(date).format('LL')
+    }
   }
 }
 </script>
+
+
+
+
+
+
+
+
 
 
 
