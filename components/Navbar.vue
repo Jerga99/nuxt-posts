0b868886180af78447941759cd1b499f366968bf
Filip Jerga <jerga99@gmail.com>
2019-07-15 11:55:06 +0200

    17 Debugging

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,7 +6,7 @@
         <a class="navbar-item" href="../">
         <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox">
         </a>
-        <div @click="isActive = !isActive"
+        <div @click="toggleIsActive"
              class="navbar-burger burger"
              aria-label="menu"
              aria-expanded="false"
@@ -16,7 +16,7 @@
           <span></span>
         </div>
       </div>
-      <div @click="isActive = !isActive"
+      <div @click="toggleIsActive"
            id="navMenu"
            :class="{'is-active': isActive}"
            class="navbar-menu">
@@ -52,7 +52,39 @@
 export default {
   data() {
     return {
-      isActive: false
+      isActive: false,
+      test1Var: 'Some Test Value',
+      test2Var: 23182,
+      test3Var: () => {
+        console.log('Calling test3Var')
+        console.log(this.test2Var)
+      },
+      test4Var: true
+    }
+  },
+  methods: {
+    toggleIsActive() {
+      this.isActive = !this.isActive
+      // this.test1()
+      // console.log(this.test1Var)
+      // console.log(this.test2Var)
+      // console.log(this.test3Var)
+      // console.log(this.test4Var)
+    },
+    test1() {
+      console.log('calling test1 function')
+      this.test1Var = 'Changed Valued!'
+      this.test2()
+    },
+    test2() {
+      console.log('calling test2 function')
+      this.test2Var = 123897213792137
+      this.test3()
+    },
+    test3() {
+      console.log('calling test3 function')
+      this.test3Var()
+      this.test4Var = false
     }
   }
 }
